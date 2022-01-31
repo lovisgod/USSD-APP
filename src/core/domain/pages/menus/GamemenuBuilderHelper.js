9354129d4b3eae888f1fe92dbd29213845046255
Ayooluwa Olosunde <ayooluwa.olosunde@getjara.co>
2022-01-31 23:30:24 +0100

    feature:fetch more pages for bet types

--- src/core/domain/pages/menus/GamemenuBuilderHelper.js
+++ src/core/domain/pages/menus/GamemenuBuilderHelper.js
@@ -217,7 +217,7 @@ class MenuBuilderHelper {
             menu.session.set('betTypes', response.games);
             let games = '';
             response.games.forEach((element) => {
-              games += `${response.games.indexOf(element) + 1}.${element.value}\n`;
+              games += `${response.games.number + 1}.${element.value}\n`;
             });
             console.log('games', games);
             menu.con(`${games}
@@ -253,7 +253,7 @@ class MenuBuilderHelper {
             menu.session.set('betTypes', betypesAlreadySaved);
             let games = '';
             response.games.forEach((element) => {
-              games += `${response.games.indexOf(element) + 1}.${element.value}\n`;
+              games += `${response.games.number + 1}.${element.value}\n`;
             });
             console.log('games', games);
             menu.con(`${games}
