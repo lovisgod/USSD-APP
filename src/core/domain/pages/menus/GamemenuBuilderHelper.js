74e9f2363592624384aa3bc32b129d90f1007fa3
Ayooluwa Olosunde <ayooluwa.olosunde@getjara.co>
2022-01-31 23:25:21 +0100

    feature:fetch more pages for bet types

--- src/core/domain/pages/menus/GamemenuBuilderHelper.js
+++ src/core/domain/pages/menus/GamemenuBuilderHelper.js
@@ -176,7 +176,7 @@ class MenuBuilderHelper {
         const code = menu.val;
         // fetch the games for the day of the week
         MainServer.getDailyGames(
-          { page: 1, limit: 10, currentWeekDay: 6 }
+          { page: 1, limit: 10, currentWeekDay: code }
         ).then((res) => {
           console.log('res', res);
           if (res.message === 'success') {
